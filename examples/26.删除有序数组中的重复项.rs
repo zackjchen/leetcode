35f2bafbcd25ba418398c74d75bfe00ef9b0e18a
zackjchen <chenjia0101@foxmail.com>
2024-08-23 01:14:56 +0800

    26.删除有序数组中的重复项

---
+++ examples/26.删除有序数组中的重复项.rs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=rust
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut fast = 0;
+        let mut slow = 0;
+        while fast < nums.len() {
+            if nums[fast] != nums[slow] {
+                slow += 1;
+                nums[slow] = nums[fast];
+            }
+            fast += 1;
+        }
+        (slow + 1) as i32
+    }
+}
+// @lc code=end
+
