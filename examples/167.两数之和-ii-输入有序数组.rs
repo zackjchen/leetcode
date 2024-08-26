45e7410d1b0feac57bd149e4286bf190899b6ec1
zackjchen <chenjia0101@foxmail.com>
2024-08-26 21:52:29 +0800

    167.两数之和, 有序数组

---
+++ examples/167.两数之和-ii-输入有序数组.rs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=rust
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let (mut i,mut j) = (0, numbers.len()-1);
+        loop {
+            if numbers[i] + numbers[j] > target {
+                j = j-1;
+            }else if numbers[i] + numbers[j] < target {
+                i = i+1;
+            }else {
+                return vec![i as i32 + 1, j as i32 + 1];
+            }
+        }
+    }
+}
+// @lc code=end
+
