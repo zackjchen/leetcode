1620283f68cac156f4f58e2b180d6f90caa0a30c
zackjchen <chenjia0101@foxmail.com>
2025-03-06 01:19:44 +0800

    240

---
+++ examples/240.搜索二维矩阵-ii.rs
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=rust
+ *
+ * [240] 搜索二维矩阵 II
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        let mut x = 0;
+        let mut y = matrix[0].len() as i32 - 1;
+        while x < matrix.len() && y >= 0 && matrix[x][y as usize] != target {
+            if y == 0{
+                return  false;
+            }
+            if matrix[x][y as usize] > target {
+                y -= 1;
+            } else {
+                x += 1;
+            }
+        }
+        if x >= matrix.len() || y < 0 {
+            false
+        }else {
+            true
+        }
+    }
+}
+// @lc code=end
+
--- examples/test.rs
+++ examples/test.rs
@@ -1,19 +1,30 @@
-use std::{collections::HashMap, vec};
+
 
 
 fn main() {
 
-    let mut matrix = vec![vec![1],vec![3]];
-    let res = search_matrix(matrix,3);
+    let matrix = vec![vec![-1,3]];
+    let res = search_matrix(matrix,-1);
     println!("res={:?}", res);
 }
 
 pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-    for nums in matrix {
-        if nums.binary_search(&target).is_ok(){
-            return true;
+    let mut x = 0;
+    let mut y = matrix[0].len() - 1;
+    while x < matrix.len() && y >= 0 && matrix[x][y as usize] != target {
+        println!("x={},y={}",x,y);
+        println!("matrix[x][y]={}",matrix[x][y as usize]);
+        if matrix[x][y as usize] > target {
+            if y == 0 {
+                return false;
+            }
+            y -= 1;
+        } else {
+            if x == matrix.len() - 1 {
+                return false;
+            }
+            x += 1;
         }
     }
-    false
-
+    true
 }
